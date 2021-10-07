f1c37460168a52341b97192ec7a00f6eb81eb9dd
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-07 14:55:58 +0100

    Trie insertion

--- 15-Tries/Trie.js
+++ 15-Tries/Trie.js
@@ -17,3 +17,35 @@ class TrieNode {
     this.endWord = false;
   }
 }
+
+class Trie {
+  construtor() {
+    this.root = new TrieNode('');
+  }
+
+  // function to get the index of a character. a being 0 and z being 25
+  getIndex(char) {
+    return char.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+
+  //   Time complexity of insertion is O(N) where N is the number of characters in the key being inserted
+
+  insert(key) {
+    if (key === null) return;
+
+    key = key.toLowerCase();
+    let currentNode = this.root;
+    let index = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      index = this.getIndex(key[i]);
+
+      if (currentNode.children[index] === null) {
+        currentNode.children[index] = new TrieNode(key[i]);
+        console.log(String(key[i]) + 'inserted');
+      }
+      currentNode = currentNode.children[index];
+    }
+    currentNode.markAsLeaf();
+  }
+}
